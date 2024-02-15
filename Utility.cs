f238b6bc7f41694203de4f4166dd9e410bd450e1
Chris-Is-Awesome <34765277+Chris-Is-Awesome@users.noreply.github.com>
2024-02-15 11:12:01 -0800

    Add Utility class

---
+++ Utility.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.SceneManagement;
+
+namespace ModCore
+{
+	public static class Utility
+	{
+		/// <summary>
+		/// Loads the scene by build index
+		/// </summary>
+		/// <param name="sceneBuildIndex">The build index</param>
+		/// <param name="additively">Should the scene be loaded additively?</param>
+		public static void LoadScene(int sceneBuildIndex, bool additively = false)
+		{
+			SceneManager.LoadScene(sceneBuildIndex, additively ? LoadSceneMode.Additive : LoadSceneMode.Single);
+		}
+
+		/// <summary>
+		/// Loads the scene by name
+		/// </summary>
+		/// <param name="sceneName">The scene name</param>
+		/// <param name="additively">Should the scene be loaded additively?</param>
+		public static void LoadScene(string sceneName, bool additively = false)
+		{
+			SceneManager.LoadScene(sceneName, additively ? LoadSceneMode.Additive : LoadSceneMode.Single);
+		}
+	}
+}

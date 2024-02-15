e28cb27d1b58b5e49caf270db58a693f6650152b
Chris-Is-Awesome <34765277+Chris-Is-Awesome@users.noreply.github.com>
2024-02-14 19:46:33 -0800

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+
+namespace ModCore
+{
+	[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+	[BepInProcess("ID2.exe")]
+	public class Plugin : BaseUnityPlugin
+	{
+		private void Awake()
+		{
+			// Plugin startup logic
+			Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+		}
+	}
+}

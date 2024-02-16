f4569f293394c4d505d38e59334bc68122962ee3
Chris-Is-Awesome <34765277+Chris-Is-Awesome@users.noreply.github.com>
2024-02-16 08:39:39 -0800

    Add key to open debug menu

--- Patches.cs
+++ Patches.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using UnityEngine;
 
 namespace ModCore
 {
@@ -7,6 +8,7 @@ namespace ModCore
 	{
 		[HarmonyPostfix]
 		[HarmonyPatch(typeof(ObjectUpdater.UpdateLayer), "SetPause")]
+		// Invokes an event hook
 		public static void ObjectUpdaterUpdateLayer_SetPause_Patch(bool pause)
 		{
 			Events.PauseChange(pause);
@@ -14,6 +16,7 @@ namespace ModCore
 
 		[HarmonyPostfix]
 		[HarmonyPatch(typeof(EntitySpawner), "DoSpawn")]
+		// Invokes an event hook
 		public static void EntitySpawner_DoSpawn_Patch(ref Entity __result)
 		{
 			Events.EntitySpawn(__result);
@@ -21,6 +24,7 @@ namespace ModCore
 
 		[HarmonyPostfix]
 		[HarmonyPatch(typeof(EntityEventsOwner), "SendDetailedDeath")]
+		// Invokes an event hook
 		public static void EntityEventsOwner_SendDetailedDeath_Patch(Entity ent, Killable.DetailedDeathData data)
 		{
 			Events.EntityDied(ent, data);
@@ -28,6 +32,7 @@ namespace ModCore
 
 		[HarmonyPostfix]
 		[HarmonyPatch(typeof(EntityEventsOwner), "SendRoomChangeDone")]
+		// Invokes an event hook
 		public static void EntityEventsOwner_SendRoomChangeDone_Patch(Entity ent, LevelRoom to, LevelRoom from, EntityEventsOwner.RoomEventData data)
 		{
 			Events.RoomChange(ent, to, from, data);
@@ -35,9 +40,21 @@ namespace ModCore
 
 		[HarmonyPostfix]
 		[HarmonyPatch(typeof(EntityEventsOwner), "SendItemGet")]
+		// Invokes an event hook
 		public static void EntityEventsOwner_SendItemGet_Patch(Entity ent, Item item)
 		{
 			Events.ItemGet(ent, item);
+		}
+
+		[HarmonyPostfix]
+		[HarmonyPatch(typeof(PauseMenu), "Update")]
+		// Allows pressing a key to open debug menu
+		public static void PausewMenu_Update_Patch(ref MenuImpl<PauseMenu> ___menuImpl)
+		{
+			if (Input.GetKeyDown(KeyCode.BackQuote))
+			{
+				___menuImpl.SwitchToScreen("debugRoot", null);
+			}
 		}
 	}
 }
